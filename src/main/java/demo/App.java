115d0de104d37c91084a036f6b668e62e223d7f7
Saisandhiya <saisandhiya02@gmail.com>
2024-03-07 17:33:11 +0530

    ME_QA_XGOOGLE_CALC1_MODULE_L1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,21 +1,24 @@
 package demo;
-import java.net.MalformedURLException;
 
+import java.net.MalformedURLException;
 
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
-        
+
         // This is to remove unnecessary warnings from your console
         System.setProperty("java.util.logging.config.file", "logging.properties");
-        
+
         TestCases tests = new TestCases(); // Initialize your test class
 
-        //TODO: call your test case functions one after other here
+        // TODO: call your test case functions one after other here
 
         tests.testCase01();
+        tests.testCase02();
+        // tests.testCase03();
 
-        //END Tests
+        // tests.testCase04();
 
+        // END Tests
 
         tests.endTest(); // End your test by clearning connections and closing browser
     }
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -77,4 +77,76 @@ public class TestCases {
         System.out.println("end Test case: testCase01");
     }
 
+    public void testCase02() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        System.out.println("Start Test case: testCase02");
+        driver.get("https://www.google.com");
+
+        // String url = driver.getCurrentUrl();
+        // String expected = "https://www.google.com";
+
+        // if (url.contains(expected)) {
+        // System.out.println("Verifying url displays");
+
+        // }
+        // System.out.println("Performing Addition Operation");
+
+        WebElement searchBar = driver.findElement(By.xpath("//textarea[@aria-label='Search']"));
+        searchBar.sendKeys("calculator");
+        searchBar.submit();
+        Thread.sleep(2000);
+        System.out.println("Performing Addition Operation");
+        WebElement valueFive = driver.findElement(By.xpath("//div[@class='PaQdxb A2W7l']/div[text()='5']"));
+        valueFive.click();
+        WebElement valuePlus = driver.findElement(By.xpath("//div[@class='PaQdxb mF5fo']/div[text()='+']"));
+        valuePlus.click();
+        WebElement valueSeven = driver.findElement(By.xpath("//div[@class='PaQdxb A2W7l']/div[text()='7']"));
+        valueSeven.click();
+        WebElement valueEqual = driver.findElement(By.xpath("//div[@class='PaQdxb mF5fo G7BHUb']/div[text()='=']"));
+        valueEqual.click();
+        WebElement result = driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span"));
+        String actualResult = result.getText();
+        String expectedResult = "12";
+
+        if (actualResult.contains(expectedResult)) {
+            System.out.println("Verifying: " + expectedResult);
+
+        }
+        WebElement removeAll = driver.findElement(By.xpath("//div[@jsname='SLn8gc']"));
+        removeAll.click();
+        Thread.sleep(2000);
+
+        System.out.println("Performing Substraction Operation");
+
+        // WebElement searchBar1 =
+        // driver.findElement(By.xpath("//textarea[@aria-label='Search']"));
+        // searchBar1.sendKeys("calculator");
+        // searchBar.submit();
+        // Thread.sleep(2000);
+        WebElement valueOne = driver.findElement(By.xpath("//div[@class='PaQdxb A2W7l']/div[text()='1']"));
+        valueOne.click();
+        WebElement valueFive5 = driver.findElement(By.xpath("//div[@class='PaQdxb A2W7l']/div[text()='5']"));
+        valueFive5.click();
+        WebElement valueMinus = driver.findElement(By.xpath("//div[@class='PaQdxb mF5fo']/div"));
+        valueMinus.click();
+        WebElement valueEight = driver.findElement(By.xpath("//div[@class='PaQdxb A2W7l']/div[text()='8']"));
+        valueEight.click();
+        WebElement valueEqualTo = driver.findElement(By.xpath("//div[@class='PaQdxb mF5fo G7BHUb']/div[text()='=']"));
+        valueEqualTo.click();
+        WebElement result1 = driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span"));
+        String actualResult1 = result1.getText();
+        String expectedResult1 = "7";
+
+        if (actualResult1.contains(expectedResult1)) {
+            System.out.println("Verifying: " + expectedResult1);
+
+        }
+        WebElement removeAll1 = driver.findElement(By.xpath("//div[@jsname='SLn8gc']"));
+        removeAll1.click();
+        Thread.sleep(2000);
+
+        System.out.println("end Test case: testCase02");
+    }
+
 }
