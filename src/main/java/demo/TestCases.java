f44b206c672c12acaa6c0058d9663b0d6cc74eb1
Saisandhiya <saisandhiya02@gmail.com>
2024-03-07 13:03:59 +0530

    ME_QA_XGOOGLE_CALC1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -9,15 +9,21 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
-    {
+
+    public TestCases() {
         System.out.println("Constructor: TestCases");
 
-        WebDriverManager.chromedriver().timeout(30).setup();
+        WebDriverManager.chromedriver().browserVersion("122.0.6261.95").setup();
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
 
@@ -33,24 +39,34 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
-    public void endTest()
-    {
+    public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
         driver.quit();
 
     }
 
-    
-    public  void testCase01(){
+    public void testCase01() throws InterruptedException {
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
-    }
 
+        WebElement searchBar = driver.findElement(By.xpath("//textarea[@aria-label='Search']"));
+        searchBar.sendKeys("calculator");
+        searchBar.submit();
+        WebElement textZero = driver.findElement(By.xpath("//div[@jsname='DjP6yd']/div"));
+
+        String title0 = textZero.getText();
+        String expected = "0";
+
+        if (title0.contains(expected)) {
+            System.out.println("Verifying: " + expected);
+
+        }
+        System.out.println("end Test case: testCase01");
+    }
 
 }
